c16c3934d5f4b702bf82d6bd52c7a297133cca16
rocodeodato <151384072+rocodeodato@users.noreply.github.com>
2025-02-19 15:39:51 -0300

    Update preloader.js

--- preloader.js
+++ preloader.js
@@ -10,74 +10,6 @@ overlay.style.cssText = `
     left: 0;
     width: 100%;
     height: 100%;
-    background-color: rgba(0, 155, 90, 1); // Fundo verde
-    z-index: 999;
-`;
-
-// Create a div for the green background behind the GIF
-const greenBackground = document.createElement('div');
-greenBackground.style.cssText = `
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    width: 100px; // Ajuste o tamanho conforme necessário
-    height: 100px; // Ajuste o tamanho conforme necessário
-    background-color: rgba(0, 155, 90, 1); // Fundo verde
-    z-index: 998; // Coloque atrás do GIF
-`;
-
-const svgImage = document.createElement('img');
-svgImage.id = 'svgImage';
-svgImage.src = 'https://apiv2.agile4pay.com/vault/CHUtpF7M/hQMVVcaJT1usnojlFV3y-BRbcVo/TDIpVQ../preloader.gif';
-svgImage.style.cssText = `
-    max-width: 100%;
-    max-height: 100%;
-    display: none;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    z-index: 999; // Coloque na frente do fundo verde
-`;
-svgImage.style.display = 'none';
-
-// Append the elements to the body
-document.body.appendChild(overlay);
-document.body.appendChild(greenBackground);
-document.body.appendChild(svgImage);
-
-// Function to hide the overlay and display the SVG
-function hideOverlay() {
-    overlay.style.display = 'none';
-    greenBackground.style.display = 'none'; // Esconder o fundo verde
-    svgImage.style.display = 'block';
-}
-
-// Add an event listener to hide the overlay when all external JS files are loaded
-document.addEventListener('DOMContentLoaded', () => {
-    // Replace the following lines with the actual code that loads your external JS files
-    // For demonstration purposes, we'll use a setTimeout to simulate loading external JS files.
-    setTimeout(hideOverlay, 100); // Replace with your actual loading code.
-});
-
-// Fallback: If all external resources are loaded and the DOMContentLoaded event doesn't fire,
-// we'll still hide the overlay when the window's load event is triggered.
-window.addEventListener('load', hideOverlay);
-
-/*
-// Create the overlay and SVG elements
-const overlay = document.createElement('div');
-overlay.id = 'loader';
-overlay.style.cssText = `
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
     //background-color: rgba(255, 255, 255, 0.7);
     background-color: rgba(0, 155, 90, 1);
     z-index: 999;
@@ -85,8 +17,8 @@ overlay.style.cssText = `
 
 const svgImage = document.createElement('img');
 svgImage.id = 'svgImage';
-//svgImage.src = 'https://xvx3-bpuh-nxxl.n7c.xano.io/vault/-WYxXyI8/NxrAmsb-iTmj8wqwfL6NwRPY0rg/ME_VGw../logo_sem_fundo_video.gif';
-svgImage.src = 'https://apiv2.agile4pay.com/vault/CHUtpF7M/hQMVVcaJT1usnojlFV3y-BRbcVo/TDIpVQ../preloader.gif';
+svgImage.src = 'https://xvx3-bpuh-nxxl.n7c.xano.io/vault/-WYxXyI8/NxrAmsb-iTmj8wqwfL6NwRPY0rg/ME_VGw../logo_sem_fundo_video.gif';
+//svgImage.src = 'https://apiv2.agile4pay.com/vault/CHUtpF7M/hQMVVcaJT1usnojlFV3y-BRbcVo/TDIpVQ../preloader.gif';
 svgImage.style.cssText = `
     max-width: 100%;
     max-height: 100%;
@@ -118,4 +50,3 @@ document.addEventListener('DOMContentLoaded', () => {
 // Fallback: If all external resources are loaded and the DOMContentLoaded event doesn't fire,
 // we'll still hide the overlay when the window's load event is triggered.
 window.addEventListener('load', hideOverlay);
-*/
